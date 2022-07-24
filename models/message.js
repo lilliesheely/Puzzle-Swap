67c27bee5ee84fbfb8ebffe67911063011bd442a
lilliesheely <lilliesheely@gmail.com>
2022-07-24 12:16:22 -0700

    Attempt 1 Message Model

--- models/message.js
+++ models/message.js
@@ -2,8 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-messageSchema = new Schema ([
-    
-])
+const messageSchema = new Schema ({
+    sender: {
+        type: Schema.Types.ObjectId, 
+        ref: "User",
+    },
+    message:{
+        text: { type:String, required:true }
+    }, 
+    users: [{
+        user: { 
+            type:mongoose.Schema.Types.ObjectId, 
+            ref:'User', 
+            required: true 
+        }
+    }],
+    sender: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref:'User', 
+        required: true 
+    },
+    read: { 
+        type: Date 
+    }
+}, {
+    timestamps: true 
+})
 
-module.exports = mongoose.model('Message', messageSchema);
